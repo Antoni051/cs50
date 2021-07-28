3dbc6aaedd5c85d4ecc3b95c9696a3712ab5aa2d
Antoni <zyla.antoni@gmail.com>
2021-07-28 20:24:50 +0100

    Image Filters

---
+++ week4Memory/problemSet/filter/filter/helpers.c
@@ -0,0 +1,126 @@
+#include "helpers.h"
+#include <math.h>
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int avgColor = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3);
+            image[i][j].rgbtRed = avgColor;
+            image[i][j].rgbtBlue = avgColor;
+            image[i][j].rgbtGreen = avgColor;
+        }
+    }
+
+    return;
+}
+
+// Convert image to sepia DONE
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int sepiaRed = round((image[i][j].rgbtRed * 0.393) + (image[i][j].rgbtGreen * 0.686) + (image[i][j].rgbtBlue * 0.189) / 3);
+            int sepiaGreen = round((image[i][j].rgbtRed * 0.349) + (image[i][j].rgbtGreen * 0.686) + (image[i][j].rgbtBlue * 0.168) / 3);
+            int sepiaBlue = round((image[i][j].rgbtRed * 0.272) + (image[i][j].rgbtGreen * 0.543) + (image[i][j].rgbtBlue * 0.131) / 3);
+
+            if (sepiaRed > 255)
+            {
+                sepiaRed = 255;
+            }
+            if (sepiaBlue > 255)
+            {
+                sepiaBlue = 255;
+            }
+            if (sepiaGreen > 255)
+            {
+                sepiaGreen = 255;
+            }
+            image[i][j].rgbtRed = sepiaRed;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue;
+        }
+    }
+    return;
+}
+
+// Reflect image horizontally DONE
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < (width / 2); j++)
+        {
+            RGBTRIPLE tmp = image[i][j];
+            image[i][j] = image[i][width - (j + 1)];
+            image[i][width - (j + 1)] = tmp;
+        }
+    }
+
+    return;
+}
+
+// Blur image
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    //create a temporary image to implement blurred algorithm on it.
+    RGBTRIPLE temp[height][width];
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int total_Red = 0;
+            int total_Blue = 0;
+            int total_Green = 0;
+            float counter = 0.00;
+
+            //Get the neighbouring pexels
+            for (int x = -1; x < 2; x++)
+            {
+                for (int y = -1; y < 2; y++)
+                {
+                    int currentX = i + x;
+                    int currentY = j + y;
+
+                    //check for valid neighbouring pixels
+                    if (currentX < 0 || currentX > (height - 1) || currentY < 0 || currentY > (width - 1))
+                    {
+                        continue;
+                    }
+
+                    //Get the image value
+                    total_Red += image[currentX][currentY].rgbtRed;
+                    total_Green += image[currentX][currentY].rgbtGreen;
+                    total_Blue += image[currentX][currentY].rgbtBlue;
+
+                    counter++;
+                }
+
+                //do the average of neigbhouring pexels
+                temp[i][j].rgbtRed = round(total_Red / counter);
+                temp[i][j].rgbtGreen = round(total_Green / counter);
+                temp[i][j].rgbtBlue = round(total_Blue / counter);
+            }
+        }
+
+    }
+
+    //copy the blurr image to the original image
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j].rgbtRed = temp[i][j].rgbtRed;
+            image[i][j].rgbtGreen = temp[i][j].rgbtGreen;
+            image[i][j].rgbtBlue = temp[i][j].rgbtBlue;
+        }
+    }
+    return;
+}
